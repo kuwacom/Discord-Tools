11eda4a3601388caedf975f57545b5c0f48c1203
kuwacom <kuwa.com3@gmail.com>
2024-02-12 16:58:36 +0900

    update: メッセージ設定をenvへ移行

--- message.js
+++ message.js
@@ -1,21 +1,16 @@
 require('dotenv').config();
 
-const channelId = ''; // メッセージ送信先チャンネルID
-
 const reply = {
-    enable: false, // リプライするか
-    targetMessageId: '', // リプライ先メッセージID
+    enable: (process.env.ENABLE_REPLY == 'true' ? true : false), // リプライするか
+    targetMessageId: process.env.REPLY_TARGET_MESSAGE_ID, // リプライ先メッセージID
 };
-
-const messageContent =
-    "test"; // リプライメッセージ
 
 const messageCount = 2; // メッセージ送信回数
 
 const sendMessages = async () => {
     try {
         const ress = await Promise.all([...Array(messageCount)].map(() => {
-            return fetch(`https://discord.com/api/v9/channels/${channelId}/messages`, {
+            return fetch(`https://discord.com/api/v9/channels/${process.env.TARGET_CHANNEL_ID}/messages`, {
                 method: 'POST',
                 headers: {
                     'Authorization': process.env.TOKEN,
@@ -23,9 +18,9 @@ const sendMessages = async () => {
                 },
                 body: JSON.stringify({
                     mobile_network_type: "unknown",
-                    content: messageContent,
+                    content: process.env.MESSAGE_CONTENTS,
                     message_reference: reply.enable ? { message_id: reply.targetMessageId } : undefined,
-                    tts: false,
+                    tts: (process.env.ENABLE_TTS == 'true' ? true : false),
                     flags: 0
                 })
             });
