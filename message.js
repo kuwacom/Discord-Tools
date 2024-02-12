3314298a77f0dd43f97c74918e1055b502a8b2e8
kuwacom <kuwa.com3@gmail.com>
2024-02-12 17:51:21 +0900

    update: コードを少し変更

--- message.js
+++ message.js
@@ -7,28 +7,30 @@ const reply = {
 
 const messageCount = 2; // メッセージ送信回数
 
-const sendMessages = async () => {
+const sendMessages = async (message) => {
+    return fetch(`https://discord.com/api/v9/channels/${process.env.TARGET_CHANNEL_ID}/messages`, {
+        method: 'POST',
+        headers: {
+            'Authorization': process.env.TOKEN,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            mobile_network_type: "unknown",
+            content: message,
+            message_reference: reply.enable ? { message_id: reply.targetMessageId } : undefined,
+            tts: (process.env.ENABLE_TTS == 'true' ? true : false),
+            flags: 0
+        })
+    });
+};
+
+(async () => {
     try {
         const ress = await Promise.all([...Array(messageCount)].map(() => {
-            return fetch(`https://discord.com/api/v9/channels/${process.env.TARGET_CHANNEL_ID}/messages`, {
-                method: 'POST',
-                headers: {
-                    'Authorization': process.env.TOKEN,
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    mobile_network_type: "unknown",
-                    content: process.env.MESSAGE_CONTENTS,
-                    message_reference: reply.enable ? { message_id: reply.targetMessageId } : undefined,
-                    tts: (process.env.ENABLE_TTS == 'true' ? true : false),
-                    flags: 0
-                })
-            });
+            return sendMessages(process.env.MESSAGE_CONTENTS);
         }));
-
         console.log(`Done send ${ress.length} Messages!`);
     } catch (error) {
         console.error('Error:', error);
     }
-};
-sendMessages();
+})();
